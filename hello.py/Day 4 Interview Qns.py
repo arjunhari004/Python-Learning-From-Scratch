ba394e13752bd4327a5df5f9fb5bd7a5c237a7f8
arjunhari004 <hariarjun658@gmail.com>
2026-08-19 14:24:43 +0530

    python

---
+++ hello.py/Day 4 Interview Qns.py
@@ -0,0 +1,19 @@
+a=360
+if(a>0):
+    if(a%2==0):
+        if(a%10==0):
+            if(a>300):
+                print('all the conditions get satisfied')
+
+b=144
+if(b>0):
+    if(b%2==0):
+        if(b%4==0):
+            print('divisible by 4')
+        elif(b%8==0):
+            print('divisible by 8')
+        elif(b%12==0):
+            print('divisible by 12')
+        else:
+            print('not positive')            
+
