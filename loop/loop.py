5afcb1b03f610ba2a1f95fbb1366947184b1fc7a
arjunhari004 <hariarjun658@gmail.com>
2026-09-01 15:05:27 +0530

    while loop problems

--- loop/loop.py
+++ loop/loop.py
@@ -1,58 +1,58 @@
-# # i=0
-# # while(i<=5):
-# #     i+=1
-# #     if(i%2!=0):
-# #         print(i)
+i=0
+while(i<=5):
+    i+=1
+    if(i%2!=0):
+        print(i)
     
 
-# # #Print number 10-1 where the numbers should be divisible only by 3
+#Print number 10-1 where the numbers should be divisible only by 3
 
-# # i=10
-# # while(i>=1):
-# #     if(i%3==0):
-# #         print(i)
-# #     i-=1   
+i=10
+while(i>=1):
+    if(i%3==0):
+        print(i)
+    i-=1   
 
-# # Print factorial of 10
+# Print factorial of 10
 
-# i = 10
-# factorial = 1  
+i = 10
+factorial = 1  
 
-# while i > 0:
-#     factorial *= i  
-#     i -= 1               
+while i > 0:
+    factorial *= i  
+    i -= 1               
 
-# print(factorial)
-
-
-# #Sum of n numbers
-
-# n = int(input("enter the number: "))
-# total_sum = 0
-# i = n
-
-# while i >= 1:
-#     total_sum += i 
-#     i -= 1          
-
-# print(total_sum)
+print(factorial)
 
 
-# # check whether the number is prime or not
+#Sum of n numbers
+
+n = int(input("enter the number: "))
+total_sum = 0
+i = n
+
+while i >= 1:
+    total_sum += i 
+    i -= 1          
+
+print(total_sum)
 
 
-# inp=int(input("Enter the number:"))
-# max=inp//2
-# flag=0
-# while max>=2:
-#     if inp%max==0:
-#         flag=1
-#         break
-#     max-=1
-# if flag==1:
-#     print("not prime")
-# else:
-#     print("Prime")        
+# check whether the number is prime or not
+
+
+inp=int(input("Enter the number:"))
+max=inp//2
+flag=0
+while max>=2:
+    if inp%max==0:
+        flag=1
+        break
+    max-=1
+if flag==1:
+    print("not prime")
+else:
+    print("Prime")        
 
 
 
