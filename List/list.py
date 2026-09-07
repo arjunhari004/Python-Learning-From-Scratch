8bd21f3409fdba0b589e56d0701338dfb94d3ca2
arjunhari004 <hariarjun658@gmail.com>
2026-09-07 14:46:14 +0530

    list

--- List/list.py
+++ List/list.py
@@ -1,8 +1,20 @@
-lst_name = [10,20,30,40,50,60,70]
+
+# list tutorial
 
 
 #built - in func
 #1.len()==number of elemnets in the lst
 
 
-print(len(lst_name))
+lisstt = [20,10,30,80,50,60,90,40,70]
+
+print(len(lisstt))
+print(lisstt)
+print(max(lisstt))
+print( min(lisstt) )
+print(sum( lisstt))
+print( sorted(lisstt))
+print( any(lisstt) )
+print(all(lisstt))
+print(lisstt[0])
+print( sorted(lisstt))
